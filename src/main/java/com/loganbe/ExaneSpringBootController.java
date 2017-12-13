7dbda7aa52efd4fd7b9515cac43916ca26505d30
Ben Logan <Ben.Logan@exanebnpparibas.com>
2017-12-13 09:04:56 +0000

    various changes

--- src/main/java/com/loganbe/ExaneSpringBootController.java
+++ src/main/java/com/loganbe/ExaneSpringBootController.java
@@ -2,17 +2,24 @@ package com.loganbe;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @EnableAutoConfiguration
 public class ExaneSpringBootController {
 
+	final static String AUTH_HEADER = "TzWJ!UD9y7s8gx54=gn*YnVKUy8mC!g%a^W+myfp";
+	
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello Ben's Exane World!";
+        return "Hello Exane World! Nothing here!";
     }
 	
     @RequestMapping("/AuthoriseUser")
@@ -23,14 +30,39 @@ public class ExaneSpringBootController {
     	String crmService = "https://cube.exane.com/service/vendors/checkuser/";
     	crmService += userEmailAddress + "/";
     	
-    	System.out.println("proxying to " + crmService);
+    	System.out.println("Proxying : " + crmService);
     	
-        return "Authenticated " + userEmailAddress;
+		RestTemplate restTemplate = new RestTemplate();
+		// we can't do this, as we need to set the auth/header
+        //CheckUser checkUser = restTemplate.getForObject("https://cube.exane.com/service/vendors/checkuser/ludovic.tenant@exane.com/", CheckUser.class);
+        
+		HttpHeaders headers = new HttpHeaders();
+		//headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+		//headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("X-Authorization", AUTH_HEADER);
+
+		//HttpEntity<RestRequest> entityReq = new HttpEntity<RestRequest>(request, headers);
+		HttpEntity<String> entityReq = new HttpEntity<String>("parameters", headers);
+		
+        ResponseEntity<CheckUser> respEntity = restTemplate
+        	    .exchange(crmService, HttpMethod.GET, entityReq, CheckUser.class);
+        
+        System.out.println(respEntity.toString());
+    	
+    	// FIXME use ExaneResponse
+        // FIXME we shouldn't be passing the input email back - injection risk etc
+    	String jsonResponse = 
+        		"{\"authorised\": \"" + respEntity.getBody().isExaneResearchAccess() + "\",\"email\":\"" + userEmailAddress + "\",\"content\":\"http://google.com\"}";
+
+    	return jsonResponse;
     }
     
 	public static void main(String[] args) {
-		System.out.println("SPRING BOOT TESTING");
+		System.out.println("Exane Spring Boot Testing (starting)");
+
 		SpringApplication.run(ExaneSpringBootController.class, args);
+        
+        System.out.println("Exane Spring Boot Testing (started)");
 	}
 
 }
